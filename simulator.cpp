ac8bd40a7c03cfe872c88beb6bd5474a61c6fbb0
supak <shupak999@gmail.com>
2022-09-30 17:09:38 +0700

    add way to covert between binary and decimal

--- simulator.cpp
+++ simulator.cpp
@@ -3,6 +3,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include<iostream>    
+#include<array> 
+using namespace std;
 
 #define NUMMEMORY 65536 /* maximum number of words in memory */
 #define NUMREGS 8       /* number of machine registers */
@@ -17,6 +20,8 @@ typedef struct stateStruct
 } stateType;
 
 void printState(stateType *);
+void binaryToDecimal(int);
+void decimalToBinary(int);
 
 int main(int argc, char *argv[])
 {
@@ -26,6 +31,7 @@ int main(int argc, char *argv[])
 
     if (argc != 2)
     {
+        cout << (argc);
         printf("error: usage: %s <machine-code file>\n", argv[0]);
         exit(1);
     }
@@ -50,8 +56,21 @@ int main(int argc, char *argv[])
         printf("memory[%d]=%d\n", state.numMemory, state.mem[state.numMemory]);
     }
 
+    // yang mai sed
+    for(int i = 0;i<state.numMemory;i++){
+
+        // if(state.mem[state.numMemory]==0){
+        //     continue;
+        // }
+
+        decimalToBinary(state.mem[i]);
+
+    }
+
     return (0);
 }
+
+
 
 void printState(stateType *statePtr)
 {
@@ -70,3 +89,37 @@ void printState(stateType *statePtr)
     }
     printf("end state\n");
 }
+
+// convert binary to decimal function
+void binaryToDecimal(int n) {
+   int decimalNumber = 0;
+   int base = 1;
+   int temp = n;
+   while (temp) {
+      int lastDigit = temp % 10;
+      temp = temp/10;
+      decimalNumber += lastDigit*base;
+      base = base*2;
+   }
+   cout<<"Decimal form of "<<n<<" is "<<decimalNumber<<endl;;
+}
+
+// convert decimal to binary function
+void decimalToBinary(int n){
+	int arr[11], i = 0, num = n;
+    
+    // Until the value of n becomes 0.
+	while(n != 0){
+		arr[i] = n % 2;
+		i++;
+		n = n / 2;
+	}
+	cout << num << " in Binary is ";
+
+	// Printing the array in Reversed Order.
+	for(i = i - 1; i >= 0;i--){
+		cout << arr[i];
+	}
+	cout << endl;
+}
+
